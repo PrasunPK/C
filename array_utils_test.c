2aacb35577b997573e31ae3d5a34a5df83b0ac95
PrasunPK <prasun_1993@rediffmail.com>
2016-01-04 15:41:56 +0530

    create tests are added .
    What changes have you made?
    
    * new tests are added.

--- array_utils_test.c
+++ array_utils_test.c
@@ -16,8 +16,17 @@ void test_areEqual_when_they_are_not_equal(){
   printf("Two array utils are not equal\n");
 }
 
+void test_create(){
+  ArrayUtil a = create(4, 5);
+
+  assert(a.length == 5);
+  assert(a.typeSize == 4);
+  printf("create creates an array util with size 5 and typeSize 4 \n");
+}
+
 int main(){
   test_areEqual_when_they_are_actually_Equal();
   test_areEqual_when_they_are_not_equal();
+  test_create();
   return 0;
 }
